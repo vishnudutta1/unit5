4bc6ba561a959b7a70e189c3270742314127c6e5
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2021-12-30 00:02:28 +0530

    new

--- newReact/newreact/src/index.js
+++ newReact/newreact/src/index.js
@@ -5,10 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <h2>Hii</h2>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
