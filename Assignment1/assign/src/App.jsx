f36c33b282cf8e9cd05a58e4a3289a016cfe5e8e
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2021-12-31 11:17:39 +0530

    react

---
+++ Assignment1/assign/src/App.jsx
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import operating from './components/operating';
+import './App.css';
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
--- newReact/newreact/src/App.jsx
+++ newReact/newreact/src/App.jsx
@@ -2,6 +2,10 @@ import ExpenseItem from './components/ExpenseItem';
 // import './App.css';
 
 function App() {
+
+  let expenseDate = new Date(2021,12,28);
+  let expenseTitle = "school Fee"
+  let expenseAmount = 300;
   return (
   <div>
     <h2>Let's start</h2>
