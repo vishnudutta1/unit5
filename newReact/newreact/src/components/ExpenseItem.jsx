887e36eed36db1e8a75f844b20208ef42d7bbd74
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-01 18:32:57 +0530

    react

--- newReact/newreact/src/components/ExpenseItem.jsx
+++ newReact/newreact/src/components/ExpenseItem.jsx
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-
+function ExpenseItem(props){
+        const month = props.date.toLocaleString('en-US')
     let title = "car insurance";
     return(
         <div className='expense-item'>
-            <div>December 30 2021</div>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
             <div className='expense_item_desc'>
                 <h2>{title}</h2>
                 <div className='expense_item_price'>$200</div>
