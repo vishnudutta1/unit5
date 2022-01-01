819abdb22b98d45126df7280df116bfddd675640
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-01 19:45:15 +0530

    react_intro_done

---
+++ react_we_session1/assign/src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import React, { Component } from 'react';
+import './components/Todo'
+import './App.css';
+import Todo from './components/Todo';
+
+class App extends Component {
+  render(){
+  return (
+  <div>
+      <Todo></Todo>
+  </div>
+  );
+}
+}
+export default App;
---
+++ react_we_session1/assign/src/components/Todo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './Todo.css'
+
+function Todo(){
+    
+   return (
+       <div> 
+           <div><h1>Mobile Operating System</h1>
+      
+      <li>Android</li>
+      <li>Blackberry</li>
+      <li>I-phone</li>
+      <li>Windpws Phone</li></div>
+             <div>  <h1>Mobile Manufacturers</h1>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Xiaomi</li></div>
+          
+          
+          
+          </div>
+      
+        
+   )
+}
+
+export default Todo;
