e2e1e7589f98b1ffc377072fa0b92d422f2bbef2
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-03 15:48:55 +0530

    react_learning

--- Class_component/pro/src/components/welcome.jsx
+++ Class_component/pro/src/components/welcome.jsx
@@ -2,7 +2,7 @@ import React,{Component} from 'react'
 
 class welcome extends Component {
     render(){
-        // return <h1>Class component</h1>
+        return <h1>Class component</h1>
     }
 }
 
