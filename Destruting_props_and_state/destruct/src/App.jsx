6633ac80928b687ea810a48d379f7cb10c59f528
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-06 01:27:46 +0530

    destruct

---
+++ Destruting_props_and_state/destruct/src/App.jsx
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import Greet from './components/Greet';
+// import { Component } from 'react';
+
+class App extends Component {
+  render()
+
+  
+ {
+  
+  return (<div>
+
+   <Greet name = 'Vishnu' heroName = 'Developer'></Greet>
+   
+  </div>
+  );
+}
+
+}
+export default App;
---
+++ Destruting_props_and_state/destruct/src/components/Greet.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Greet = (name,heroName) =>{
+    
+
+    return(
+        <div>
+            <h1>Hello{name} A.K.S {heroName} </h1>
+            </div>
+    )
+
+}
+export default Greet
