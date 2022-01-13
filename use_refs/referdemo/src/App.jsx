c2d3fcb06ee35f39cf36cc36d2600b6ed5a3c8dc
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-13 19:27:18 +0530

    ref

---
+++ use_refs/referdemo/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import RefDemo from './components/RefDemo';
+
+
+function App() {
+  return (
+    <div className="App">
+    <RefDemo></RefDemo>
+    </div>
+  );
+}
+
+export default App;
---
+++ use_refs/referdemo/src/components/RefDemo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class RefDemo extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" />
+            </div>
+        )
+    }
+}
+
+export default RefDemo
