d96695dfcef1609c36e2445a78c49396198264b4
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-20 19:51:08 +0530

    practice

--- Class_component/pro/src/App.jsx
+++ Class_component/pro/src/App.jsx
@@ -7,32 +7,24 @@ import { Component } from 'react';
 // import IntroJSX from './components/IntroJSX';
 
 
-class App extends Component {
-  render()
-
-  
- {
+function App() {
   
   return (<div>
     <Greet name = "vishnu">
     <p>We are Best</p>
     </Greet>
-    <Greet name = "abhi">
-      <button>submit</button>
-    </Greet>
+    
     <Greet name = "dheeraj">
     <p>we will secure a high package job</p>
     </Greet>
     
-    <Greet name = "Anand">
-     <button>Accepted</button>
-    </Greet>
+  
    
   </div>
   );
 }
 
-}
+
 
 
 export default App;
